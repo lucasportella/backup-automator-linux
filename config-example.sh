d2db0c57e4df1918ea63d5ac9806534bb8e090d3
lucas <lucasportellaagu@gmail.com>
2023-10-17 22:38:46 -0300

    update config-example variables

--- config-example.sh
+++ config-example.sh
@@ -3,13 +3,12 @@ export samba_user="john" # not best practise, samba_user should be the same as s
 export system_user="mike"
 export samba_password="password"
 
-export log_dir="/home/$user/logs"
+export log_dir="/home/$system_user/logs"
 
 # Source directory
-export shared_folder_dir="/home/$user/my-shared-folder"
-export source_dir="/home/$user/my-src-dir"
-export source_mount_target="/home/my-mount-target"
+export shared_dir_source="//mike-mikepc12345.local/sambashare"
+export shared_dir_mount_target="/home/$system_user/backup/shared-folder"
 
 # Destination directory
-export destination_dir="/run/media/john/my-device"
-export mount_device_location="/dev/sda1"
+export destination_mount_device_location="/dev/sda1"
+export destination_dir="/home/$system_user/backup/$system_user-destination-backup/my-backup"
